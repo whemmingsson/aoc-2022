d4d3d188d7709c85402346c0484b647a41dead0c
William <william.hemmingsson@gmail.com>
2022-12-03 12:00:23 +0100

    Day 3 - Somewhat more compact

--- 03/index.js
+++ 03/index.js
@@ -1,38 +1,21 @@
 const rows = require("../util/loader.js").getStrings("data").map((r) => r.trim());
 
 // Part 1
-const isUpperCase = (item) => {
-    return item.toUpperCase() === item;
-}
-
 const prioOf = (item) => {
-    const ascii = item.charCodeAt(0);
-    if (isUpperCase(item)) {
-        return ascii - 38;
-    }
-    return ascii - 96;
+    return item.charCodeAt(0) - (item.toUpperCase() === item ? 38 : 96);
 }
 
 const findCommon = (c1, c2) => {
-    const intersection = c1.split("").filter(x => c2.split("").includes(x));
-    return intersection[0];
+    return c1.split("").filter(x => c2.split("").includes(x))[0];
 }
 
-let sum = 0;
-for (let i = 0; i < rows.length; i++) {
-    const index = rows[i].length / 2;
-    var comp1 = rows[i].substr(0, index);
-    var comp2 = rows[i].substr(index);
+console.log("Part 1:",
+    rows.reduce((p, c) => (p + prioOf(findCommon(c.substr(0, c.length / 2), c.substr(c.length / 2)))), 0));
 
-    sum += prioOf(findCommon(comp1, comp2));
-}
-
-console.log("Part 1", sum);
 
 // Part 2
 const findCommonThree = (c1, c2, c3) => {
-    const intersection = c1.split("").filter(x => c2.split("").includes(x) && c3.split("").includes(x));
-    return intersection[0];
+    return c1.split("").filter(x => c2.split("").includes(x) && c3.split("").includes(x))[0];
 }
 
 sum = 0;
