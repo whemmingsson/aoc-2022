6f3272169fe8ee8c67e9e73340a681370da97b6e
William <william.hemmingsson@gmail.com>
2022-12-04 13:00:11 +0100

    Day 4 - Cleanup

--- 04/index.js
+++ 04/index.js
@@ -1,12 +1,7 @@
 const rows = require("../util/loader.js").getStrings("data").map((r) => r.trim());
 
-const isSubsetOf = (a, b) => {
-    return (b.f >= a.f && b.t <= a.t)
-}
-
-const overlaps = (a, b) => {
-    return b.f <= a.t && a.f <= b.t;
-}
+const isSubsetOf = (a, b) => b.f >= a.f && b.t <= a.t || a.f >= b.f && a.t <= b.t
+const overlaps = (a, b) => b.f <= a.t && a.f <= b.t;
 
 const createRange = (r) => {
     const parts = r.split("-");
@@ -19,7 +14,7 @@ for (let pair of rows) {
     const ranges = pair.split(",");
     const r1 = createRange(ranges[0]);
     const r2 = createRange(ranges[1]);
-    countP1 += isSubsetOf(r1, r2) || isSubsetOf(r2, r1) ? 1 : 0;
+    countP1 += isSubsetOf(r1, r2) ? 1 : 0;
     countP2 += overlaps(r1, r2) ? 1 : 0;
 }
 
