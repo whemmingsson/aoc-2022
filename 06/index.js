afe0f712685d1803297898333a26d43324a31fae
William Hemmingsson <william.hemmingsson@storytel.com>
2022-12-06 08:22:01 +0100

    Day 6 - Refactored version

--- 06/index.js
+++ 06/index.js
@@ -1,29 +1,19 @@
 const rows = require("../util/loader.js").getStrings("data");
 const stream = rows[0].split("");
 
-console.log(stream);
+const MARKER_SIZE = 4;
 
-const isProperMarker = (marker) => {
-  const set = new Set(marker);
-  return set.size === 14;
-};
+let marker = [];
+let i = 0;
+let valid = false;
 
-let currentMarker = [];
-let markerPos = -1;
-for (let i = 0; i < stream.length; i++) {
-  let c = stream[i];
-
-  if (currentMarker.length < 14) {
-    currentMarker.push(c);
-  } else if (currentMarker.length === 14) {
-    currentMarker = currentMarker.slice(1);
-    currentMarker.push(c);
-    const valid = isProperMarker(currentMarker);
-    if (valid) {
-      markerPos = i + 1;
-      break;
-    }
+do {
+  marker.push(stream[i]);
+  if (marker.length === MARKER_SIZE + 1) {
+    marker.splice(0, 1);
+    valid = new Set(marker).size === MARKER_SIZE;
   }
-}
+  i++;
+} while (!valid);
 
-console.log("Part 2", currentMarker, markerPos);
+console.log(i);
