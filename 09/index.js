a2570b4e0cb024d5b9cad979edb4736fc74bc435
William Hemmingsson <william.hemmingsson@storytel.com>
2022-12-09 16:57:45 +0100

    Day 9 - Moved to a array based solution

--- 09/index.js
+++ 09/index.js
@@ -1,82 +1,96 @@
 const rows = require("../util/loader.js").getStrings("data");
 console.log(rows.length);
 
+class Knot {
+  constructor() {
+    this.current = { x: 0, y: 0 };
+    this.prev = { x: 0, y: 0 };
+  }
+
+  advance() {
+    this.prev.x = this.current.x;
+    this.prev.y = this.current.y;
+  }
+}
+
 // Store values as x,y -> true
 const visited = {};
 const SQRT_OF_TWO = Math.sqrt(2);
+
+const head = new Knot();
+const tail = new Knot();
+const middle = new Knot();
+
+const knots = [head, tail];
+
+updatePrev = () => {
+  for (let i = 0; i < knots.length - 1; i++) {
+    knots[i].advance();
+  }
+};
+
+const followLogic = () => {
+  for (let i = 0; i < knots.length - 1; i++) {
+    const h = knots[i];
+    const t = knots[i + 1];
+    if (shouldFollow(h, t)) {
+      follow(h, t);
+    }
+  }
+
+  visit(knots[knots.length - 1].current.x, knots[knots.length - 1].current.y);
+};
 
 const visit = (x, y) => {
   const p = `${x},${y}`;
   if (!visited[p]) visited[p] = true;
 };
 
-const follow = () => {
-  tail.x = headPrev.x;
-  tail.y = headPrev.y;
-  visit(tail.x, tail.y);
+const follow = (head, tail) => {
+  tail.current.x = head.prev.x;
+  tail.current.y = head.prev.y;
 };
 
-const head = { x: 5, y: 5 };
-const headPrev = { x: 5, y: 5 };
-const tail = { x: 5, y: 5 };
+const distance = (a, b) => {
+  const xDiff = Math.abs(a.current.x - b.current.x);
+  const yDiff = Math.abs(a.current.y - b.current.y);
+  return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+};
 
-updatePrev = () => {
-  headPrev.x = head.x;
-  headPrev.y = head.y;
+const shouldFollow = (head, tail) => {
+  return distance(head, tail) > SQRT_OF_TWO;
 };
 
 const moveRight = (steps) => {
   for (let i = 0; i < steps; i++) {
     updatePrev();
-    head.x += 1;
-
-    if (shouldFollow()) {
-      follow();
-    }
+    head.current.x += 1;
+    followLogic();
   }
 };
 
 const moveLeft = (steps) => {
   for (let i = 0; i < steps; i++) {
     updatePrev();
-    head.x -= 1;
-
-    if (shouldFollow()) {
-      follow();
-    }
+    head.current.x -= 1;
+    followLogic();
   }
 };
 
 const moveUp = (steps) => {
   for (let i = 0; i < steps; i++) {
     updatePrev();
-    head.y -= 1;
-
-    if (shouldFollow()) {
-      follow();
-    }
+    head.current.y -= 1;
+    followLogic();
   }
 };
 
 const moveDown = (steps) => {
   for (let i = 0; i < steps; i++) {
     updatePrev();
-    head.y += 1;
-
-    if (shouldFollow()) {
-      follow();
-    }
+    head.current.y += 1;
+    followLogic();
   }
-};
-
-const distance = () => {
-  const xDiff = Math.abs(head.x - tail.x);
-  const yDiff = Math.abs(head.y - tail.y);
-  return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-};
-
-const shouldFollow = () => {
-  return distance() > SQRT_OF_TWO;
 };
 
 const map = {
@@ -86,7 +100,6 @@ const map = {
   D: moveDown,
 };
 
-visit(5, 5);
 for (let instruction of rows) {
   const parts = instruction.split(" ");
   const direction = parts[0];
